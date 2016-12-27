b02e9ebac99b60c40ad28413cab4541ebf414569
Rohnit <rntpadariya38@gmail.com>
2016-12-27 10:56:26 +0530

    Initiate CallSwipe

---
+++ app/src/main/java/co/ronit/sample/MainActivity.java
@@ -0,0 +1,32 @@
+package co.ronit.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import co.aenterhy.sample.R;
+import co.ronit.toggleswitch.ToggleSwitchButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleSwitchButton toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggle = (ToggleSwitchButton) findViewById(R.id.toggle);
+        toggle.setOnTriggerListener(new ToggleSwitchButton.OnTriggerListener() {
+            @Override
+            public void toggledUp() {
+                Toast.makeText(MainActivity.this, "Video", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void toggledDown() {
+                Toast.makeText(MainActivity.this, "Audio", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
